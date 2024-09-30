d9b8c191fc0249d19b4ab6ab6f555dc1de2fdefa
Hinoyat <hhwj2270@gmail.com>
2024-09-30 17:52:09 +0900

    240930 1742

--- acmicpc/Graph/GOLD/11404 Floyd.py
+++ acmicpc/Graph/GOLD/11404 Floyd.py
@@ -3,5 +3,11 @@
 N = int(input())
 M = int(input())
 
-GRAPH = [[] for _ in range(N + 1)]
-# fir
+Graph = [[] for _ in range(N + 1)]
+for i in range(M):
+    a, b, val = map(int, input().split())
+    Graph[a].append((val, b))
+
+for  i in range(1, N+1):
+    for j in range(1, N + 1):
+        pass
